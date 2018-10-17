80642a159f1ba459a27e3d7c9e072fcfc3396fb7
Rakesh Kumar Mallick <rkm.bitmesra@gmail.com>
2018-10-17 23:09:59 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# MyCRM
+Customer Relationship Management system or CRM
+
+A full working Spring MVC and Hibernate app that connects to a database.This application will basically allow us to track customers,add new customers, update, delete,and also list all the customers in our database.
---
+++ src/main/java/com/rkmcoder/spring/dao/TestJDBC.java
@@ -0,0 +1,29 @@
+package com.rkmcoder.spring.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJDBC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false";
+		String user = "springstudent";
+		String pass = "pass123";
+		
+		try {
+			System.out.println("Connecting to database: " + jdbcUrl);
+			
+			Connection myConn =
+					DriverManager.getConnection(jdbcUrl, user, pass);
+			
+			System.out.println("Connection successful!!!");
+			
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+
+	}
+
+}
